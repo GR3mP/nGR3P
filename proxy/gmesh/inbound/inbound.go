ab1464d029a6f4ad4f94f77a1560dbff7c8d7964
obitotsky <glebobitotsky@yandex.com>
2026-04-22 20:05:56 +1000

    update inbound proxy/gmesh

--- proxy/gmesh/inbound/inbound.go
+++ proxy/gmesh/inbound/inbound.go
@@ -1,1 +1,24 @@
 package inbound
+
+import (
+	"context"
+
+	"github.com/xtls/xray-core/common"
+	"github.com/xtls/xray-core/core"
+	"github.com/xtls/xray-core/features/dns"
+)
+
+func init() {
+	common.Must(common.RegisterConfig((*Config)(nil), func(ctx context.Context, config interface{}) (interface{}, error) {
+		var dc dns.Client
+		if err := core.RequireFeatures(ctx, func(d dns.Client) error {
+			dc = d
+			return nil
+		}); err != nil {
+			return nil, err
+		}
+
+		// c := config.(*Config)
+		return dc, nil // !!!!! PLUG
+	}))
+}
